dc48b9530fa2ade2c5643c7a4c4a821a72d32b8d
Felipe Menanteau <felipe@illinois.edu>
2022-05-25 13:30:10 -0500

    Update docker image registry location

--- src/broker/docker/build.sh
+++ src/broker/docker/build.sh
@@ -3,5 +3,6 @@ export BROKER_USER=broker
 export repo_name=menanteau
 export TAG=latest
 export image=deep_broker
+repo_name=registry.gitlab.com/antares-at-ncsa/scimma-alerts-integration-demo
 docker build -t $repo_name/$image:$TAG \
        --build-arg BROKER_USER --rm=true .
