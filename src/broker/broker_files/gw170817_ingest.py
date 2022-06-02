223e512ae5e135b6d4ee2437edfaa19d2404d86a
Deep Chatterjee <deep.chatterjee@ligo.org>
2022-06-02 10:23:30 -0500

    move ingestor script to deep_broker service

--- src/broker/broker_files/gw170817_ingest.py
+++ src/broker/broker_files/gw170817_ingest.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-print("Inserting data from DECam to photometry table...")
 
 import os, time
+import db_utils, utils
 
-import utils, db_utils
-
+print("Inserting data into photometry table...")
 
 MARIADB_HOSTNAME = os.getenv('MARIADB_SERVICE_NAME')
 MARIADB_DATABASE = os.getenv('MARIADB_DATABASE')
@@ -12,6 +11,7 @@ MARIADB_USER = os.getenv('MARIADB_USER')
 MARIADB_PASSWORD = os.getenv('MARIADB_PASSWORD')
 
 WAIT_TIME = 0.1
+
 
 conn = db_utils.DbConnector(MARIADB_HOSTNAME, MARIADB_USER,
                             MARIADB_PASSWORD, MARIADB_DATABASE)
@@ -22,8 +22,12 @@ decam_g_band, decam_r_band, decam_i_band = utils.get_photometry('DECam')
 decam_data = sorted(decam_g_band + decam_r_band + decam_i_band,
                     key=lambda e: e['time'])
 
-# for data in decam_data:
-#     assert conn.photometry_table_cols.issubset(set(data))
-#     conn.insert_photometry_data(
-#         {k:data[k] for k in conn.photometry_table_cols})
-#     time.sleep(WAIT_TIME)
+for num, data in enumerate(decam_data, 1):
+    assert conn.photometry_table_cols.issubset(set(data))
+    conn.insert_photometry_data(
+        {k:data[k] for k in conn.photometry_table_cols})
+    time.sleep(WAIT_TIME)
+    if num % 100 == 0:
+        print(f"Inserted {num} entries")
+
+conn.close_db_connection()
